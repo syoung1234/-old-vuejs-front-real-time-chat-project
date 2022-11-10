ab281fce1463f2276d1567cbe83bf31bd3487207
syoung1234 <syoung8818@gmail.com>
2022-11-10 22:15:13 +0900

    Add proxy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
 const webpack = require('webpack');
-
+const target = 'http://127.0.0.1:8080';
 module.exports = {
+  devServer: {
+    proxy: {
+        //proxy 요청을 보낼 api 시작 부분
+        '/api': {
+            target,
+            changeOrigin: true
+        }
+    }
+  },
   configureWebpack: {
     // Set up all the aliases we use in our app.
     plugins: [
@@ -19,5 +28,6 @@ module.exports = {
   css: {
     // Enable CSS source maps.
     sourceMap: process.env.NODE_ENV !== 'production'
-  }
+  },
+  
 };
